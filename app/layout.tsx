a82af3ecb34c6a9cf22a80f0ac955a5531d0f2b5
jcollicoat <jcollicoat@gmail.com>
2025-09-21 18:50:46 +0100

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Inter, Urbanist } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
+const urbanist = Urbanist({
+    variable: "--font-urbanist",
+    subsets: ["latin"],
+});
+
+const inter = Inter({
+    variable: "--font-inter",
     subsets: ["latin"],
 });
 
@@ -26,7 +31,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${urbanist.variable} ${inter.variable} ${geistMono.variable} font-sans antialiased`}
             >
                 <Header />
                 {children}
