7a64bc467bf34f31b1de27689640631bec97d7a1
jcollicoat <jcollicoat@gmail.com>
2025-09-23 23:04:48 +0100

    initial beer options

--- components/ProductTile.tsx
+++ components/ProductTile.tsx
@@ -2,10 +2,41 @@ import { Product } from "@/types/product";
 
 export function ProductTile({ product }: { product: Product }) {
     return (
-        <div className="grid rounded-md bg-gray-50 p-2">
+        <div className="grid grid-cols-1 gap-2 rounded-md border border-gray-100 bg-gray-50 p-2">
+            <div className="aspect-square rounded-s bg-white"></div>
             <h3 className="line-clamp-2 text-sm font-semibold">
                 {product.name}
             </h3>
+            {"beerOptions" in product && (
+                <>
+                    <div className="flex flex-wrap gap-1 text-xs text-gray-500">
+                        <span>
+                            {product.beerOptions.size.value}
+                            {product.beerOptions.size.unit}
+                        </span>
+                        <span>{product.beerOptions.container}</span>
+                    </div>
+                    <div className="flex flex-wrap gap-1 text-xs text-gray-500">
+                        <span>
+                            {product.beerOptions.quantity.length === 1
+                                ? "Quantity:"
+                                : "Quantities:"}
+                        </span>
+                        <span>
+                            {product.beerOptions.quantity
+                                .map((q) => q)
+                                .join(", ")}
+                        </span>
+                    </div>
+                    <div className="flex flex-wrap gap-1 text-xs text-gray-500">
+                        {product.beerOptions.abv}% ABV
+                    </div>
+                </>
+            )}
+            <span className="font-bold text-red-700">
+                ${product.price}
+                <span className="ml-1 text-xs font-semibold">+GST</span>
+            </span>
         </div>
     );
 }
