8770cd040941fa4e91ffcfb25e6e28f7d6926b9d
jcollicoat <jcollicoat@gmail.com>
2025-10-01 23:15:34 +0100

    adjust layout

--- app/beers/[id]/page.tsx
+++ app/beers/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Home, ShoppingCart } from "lucide-react";
+import { Check, Home, ShoppingCart } from "lucide-react";
 import { notFound } from "next/navigation";
 import { BeerBadges } from "@/components/BeerBadges";
 import { DisplayPrice } from "@/components/DisplayPrice";
@@ -12,15 +12,9 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 import { sanity } from "@/lib/sanity";
+import { cn } from "@/lib/utils";
 import { Beer } from "@/types/product";
 
 export default async function Page({ params }: { params: { id: string } }) {
@@ -66,7 +60,7 @@ export default async function Page({ params }: { params: { id: string } }) {
                 <div className="overflow-hidden rounded-lg border">
                     <ProductImage product={beer} />
                 </div>
-                <div className="grid h-min gap-4 md:pt-3 lg:pt-6 lg:pr-3 lg:pb-6">
+                <div className="grid h-min gap-6 md:pt-3 lg:pt-6 lg:pr-3 lg:pb-6">
                     <div className="grid gap-2">
                         <span className="text-xs text-gray-500">
                             {beer.code}
@@ -77,49 +71,40 @@ export default async function Page({ params }: { params: { id: string } }) {
                         <span className="text-gray-500">
                             {beer.company.name}
                         </span>
-                        <BeerBadges beerOptions={beer.beerOptions} />
-                    </div>
-                    <div className="grid gap-1">
-                        <label className="text-xs font-light text-gray-500">
-                            Pack Size
-                        </label>
-                        <Select>
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Pack Size..." />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {beer.beerOptions.quantity.map((o) => (
-                                    <SelectItem key={o} value={String(o)}>
-                                        {o} Pack
-                                    </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                    </div>
-                    <div className="grid gap-1">
-                        <label className="text-xs font-light text-gray-500">
-                            Quantity
-                        </label>
-                        <Input
-                            type="number"
-                            defaultValue={beer.quantity[0]}
-                            min={1}
-                            max={beer.quantity[beer.quantity.length - 1]}
-                        />
-                    </div>
-                    <Separator />
-                    <div className="grid grid-cols-2 gap-3">
                         <span className="font-display mt-auto text-2xl font-bold text-red-700">
                             <DisplayPrice price={beer.price} />{" "}
                             <span className="text-xs font-semibold">+GST</span>
                         </span>
+                        <BeerBadges beerOptions={beer.beerOptions} />
+                    </div>
+                    <div className="flex flex-wrap gap-2">
+                        {beer.beerOptions.quantity.map((o, i) => (
+                            <Button key={o} variant="outline">
+                                <Check className={cn(i !== 0 && "opacity-0")} />
+                                {o} Pack
+                            </Button>
+                        ))}
+                    </div>
+                    <div className="grid grid-cols-2 items-end gap-3">
+                        <div className="grid gap-1">
+                            <label className="text-xs font-light text-gray-500">
+                                Quantity
+                            </label>
+                            <Input
+                                type="number"
+                                defaultValue={beer.quantity[0]}
+                                min={1}
+                                max={beer.quantity[beer.quantity.length - 1]}
+                            />
+                        </div>
                         <Button>
                             Add to Cart <ShoppingCart />
                         </Button>
                     </div>
                 </div>
             </div>
-            <div className="mx-auto mt-12 w-full max-w-3xl">
+            <div className="mx-auto w-full max-w-3xl">
+                <Separator className="mb-6" />
                 <h2 className="font-display text-2xl font-bold">
                     Product Details
                 </h2>
