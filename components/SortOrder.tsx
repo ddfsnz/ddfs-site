a5103229a2f203f86a08d5221134ec98efba957d
jcollicoat <jcollicoat@gmail.com>
2025-10-02 23:25:02 +0100

    use popover for sort options

--- components/SortOrder.tsx
+++ components/SortOrder.tsx
@@ -1,12 +1,13 @@
 "use client";
+import { ArrowDownWideNarrow } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 
 const SORT_OPTIONS = [
     { label: "Name", value: "name asc" },
@@ -20,9 +21,13 @@ const SORT_OPTIONS = [
 export function SortOrder() {
     const router = useRouter();
     const searchParams = useSearchParams();
-    const current = searchParams.get("sort") || SORT_OPTIONS[0].value;
+    const [sort, setSort] = useState(
+        searchParams.get("sort") || SORT_OPTIONS[0].value,
+    );
+    const [open, setOpen] = useState(false);
 
     function handleChange(value: string) {
+        setSort(value);
         const params = new URLSearchParams(Array.from(searchParams.entries()));
         if (value === "name asc") {
             params.delete("sort");
@@ -30,20 +35,30 @@ export function SortOrder() {
             params.set("sort", value);
         }
         router.replace(`?${params.toString()}`);
+        setOpen(false);
     }
 
     return (
-        <Select value={current} onValueChange={handleChange}>
-            <SelectTrigger size="sm" className="w-[180px]">
-                <SelectValue placeholder="Sort Order" />
-            </SelectTrigger>
-            <SelectContent>
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button size="sm" variant="ghost" className="text-xs">
+                    {SORT_OPTIONS.find((o) => o.value === sort)?.label}{" "}
+                    <ArrowDownWideNarrow />{" "}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent align="end" className="flex w-min flex-col p-1">
                 {SORT_OPTIONS.map((o) => (
-                    <SelectItem key={o.value} value={o.value}>
+                    <Button
+                        key={o.value}
+                        onClick={() => handleChange(o.value)}
+                        size="sm"
+                        variant="ghost"
+                        className="justify-start text-xs"
+                    >
                         {o.label}
-                    </SelectItem>
+                    </Button>
                 ))}
-            </SelectContent>
-        </Select>
+            </PopoverContent>
+        </Popover>
     );
 }
