11db942bebdbfda2c808787e2d669fdf98ad249e
jcollicoat <jcollicoat@gmail.com>
2025-10-02 23:12:53 +0100

    add mobile filters

--- app/beers/page.tsx
+++ app/beers/page.tsx
@@ -1,8 +1,15 @@
+import { Settings2 } from "lucide-react";
 import { CatalogHeader } from "@/components/CatalogHeader";
 import { FilterInput } from "@/components/FilterInput";
 import { ProductGrid } from "@/components/ProductGrid";
 import { SearchInput } from "@/components/SearchInput";
 import { SortOrder } from "@/components/SortOrder";
+import { Button } from "@/components/ui/button";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 import { BEERS_CATEGORY_ID, sanity } from "@/lib/sanity";
 import { Company, Style } from "@/types/metadata";
 import { Beer } from "@/types/product";
@@ -96,7 +103,7 @@ export default async function Page({
                             initialValue={filters.search}
                         />
                         <FilterInput
-                            label="Producers"
+                            label="Producer"
                             filterName="company"
                             filterOptions={companies.map((c) => ({
                                 value: c._id,
@@ -112,22 +119,70 @@ export default async function Page({
                             }))}
                         />
                         <FilterInput
-                            label="Sizes"
+                            label="Size"
                             filterName="size"
                             filterOptions={sizes}
                         />
                         <FilterInput
-                            label="Containers"
+                            label="Container"
                             filterName="container"
                             filterOptions={containers}
                         />
                     </div>
                 </div>
                 <div>
-                    <div className="flex items-center justify-between border-b bg-gray-50 p-1 pl-4">
-                        <span className="text-sm font-medium text-gray-500">
-                            {beers.length} Products
-                        </span>
+                    <div className="flex items-center justify-between border-b bg-gray-50 p-1 md:pl-3">
+                        <div className="flex items-center gap-3">
+                            <Popover>
+                                <PopoverTrigger
+                                    className="block md:hidden"
+                                    asChild
+                                >
+                                    <Button size="icon" variant="ghost">
+                                        <Settings2 />
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent>
+                                    <div className="sticky top-24 grid grid-cols-1 place-content-start gap-4">
+                                        <SearchInput
+                                            label="Search"
+                                            initialValue={filters.search}
+                                        />
+                                        <FilterInput
+                                            label="Producer"
+                                            filterName="company"
+                                            filterOptions={companies.map(
+                                                (c) => ({
+                                                    value: c._id,
+                                                    label: c.name,
+                                                }),
+                                            )}
+                                        />
+                                        <FilterInput
+                                            label="Style"
+                                            filterName="style"
+                                            filterOptions={styles.map((s) => ({
+                                                value: s._id,
+                                                label: s.name,
+                                            }))}
+                                        />
+                                        <FilterInput
+                                            label="Size"
+                                            filterName="size"
+                                            filterOptions={sizes}
+                                        />
+                                        <FilterInput
+                                            label="Container"
+                                            filterName="container"
+                                            filterOptions={containers}
+                                        />
+                                    </div>
+                                </PopoverContent>
+                            </Popover>
+                            <span className="text-sm font-medium text-gray-500">
+                                {beers.length} Products
+                            </span>
+                        </div>
                         <SortOrder />
                     </div>
                     <div className="py-2 md:p-2 md:pr-0 lg:p-3 lg:pr-0">
