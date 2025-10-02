142869b9cc20a8f005191b690c1fddcfae70603d
jcollicoat <jcollicoat@gmail.com>
2025-10-02 20:46:49 +0100

    implement initial filters layout

--- app/beers/page.tsx
+++ app/beers/page.tsx
@@ -1,6 +1,7 @@
 import { CatalogHeader } from "@/components/CatalogHeader";
 import { ProductGrid } from "@/components/ProductGrid";
 import { SortOrder } from "@/components/SortOrder";
+import { Input } from "@/components/ui/input";
 import { BEERS_CATEGORY_ID, sanity } from "@/lib/sanity";
 import { Beer } from "@/types/product";
 
@@ -30,13 +31,22 @@ export default async function Page({
     return (
         <>
             <CatalogHeader heading="Beers" />
-            <div className="flex items-center justify-between border-b bg-gray-50 p-1">
-                <span className="text-sm font-medium text-gray-500">
-                    {beers.length} Products
-                </span>
-                <SortOrder />
+            <div className="grid grid-cols-1 md:grid-cols-[240px_auto]">
+                <div className="hidden gap-4 border-r p-3 md:grid">
+                    <Input placeholder="Search..." />
+                </div>
+                <div>
+                    <div className="flex items-center justify-between border-b bg-gray-50 p-1 pl-4">
+                        <span className="text-sm font-medium text-gray-500">
+                            {beers.length} Products
+                        </span>
+                        <SortOrder />
+                    </div>
+                    <div className="py-2 md:p-2 md:pr-0 lg:p-3 lg:pr-0">
+                        <ProductGrid products={beers} />
+                    </div>
+                </div>
             </div>
-            <ProductGrid products={beers} />
         </>
     );
 }
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -3,7 +3,7 @@ import { Product } from "@/types/product";
 
 export function ProductGrid({ products }: { products: Product[] }) {
     return (
-        <div className="mt-2 grid grid-cols-2 gap-2 sm:grid-cols-3 lg:mt-3 lg:grid-cols-4 lg:gap-3">
+        <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 xl:grid-cols-4 xl:gap-3">
             {products.map((p) => (
                 <ProductGridTile key={p._id} product={p} />
             ))}
