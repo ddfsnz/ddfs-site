eb21e041728006a05dfdac91e0819b28e94ca767
jcollicoat <jcollicoat@gmail.com>
2025-11-06 23:04:53 +0000

    product catalog for tobacco

--- app/tobacco/page.tsx
+++ app/tobacco/page.tsx
@@ -1,11 +1,35 @@
 import { CatalogHeader } from "@/components/CatalogHeader";
-import { ProductGrid } from "@/components/ProductGrid";
+import { FilterInput } from "@/components/FilterInput";
+import { ProductCatalog } from "@/components/ProductCatalog";
 import { sanity, TOBACCO_CATEGORY_ID } from "@/lib/sanity";
+import { Company, Style } from "@/types/metadata";
 import { Tobacco } from "@/types/product";
 
-export default async function Page() {
+export default async function Page({
+    searchParams,
+}: {
+    searchParams: {
+        sort?: string;
+        search?: string;
+        company?: string;
+        style?: string;
+    };
+}) {
+    const filters = await searchParams;
+    let searchFilter = filters.search
+        ? `&& (name match "${filters.search}*" || company->name match "${filters.search}*")`
+        : "";
+    if (filters.company) {
+        searchFilter += ` && company._ref == "${filters.company}"`;
+    }
+    if (filters.style) {
+        searchFilter += ` && tobaccoOptions.style._ref == "${filters.style}"`;
+    }
+
+    const sortOrder = filters.sort || "name asc";
+
     const tobaccos = await sanity.fetch<Tobacco[]>(
-        `*[_type == "product" && category._ref == "${TOBACCO_CATEGORY_ID}"]{
+        `*[_type == "product" && category._ref == "${TOBACCO_CATEGORY_ID}" ${searchFilter}] | order(${sortOrder}) {
             ...,
             company->{
                 ...,
@@ -21,10 +45,37 @@ export default async function Page() {
         }`,
     );
 
+    const companies = await sanity.fetch<Company[]>(
+        `*[_type == "company" && category._ref == "${TOBACCO_CATEGORY_ID}"] | order(name asc)`,
+    );
+
+    const styles = await sanity.fetch<Style[]>(
+        `*[_type == "tag" && type == "style" && category._ref == "${TOBACCO_CATEGORY_ID}"] | order(name asc)`,
+    );
+
     return (
         <>
             <CatalogHeader heading="Tobacco" />
-            <ProductGrid products={tobaccos} />
+            <ProductCatalog products={tobaccos} search={filters.search}>
+                <FilterInput
+                    label="Producer"
+                    filterName="company"
+                    filterOptions={companies.map((c) => ({
+                        value: c._id,
+                        label: c.name,
+                    }))}
+                    defaultValue={filters.company}
+                />
+                <FilterInput
+                    label="Style"
+                    filterName="style"
+                    filterOptions={styles.map((s) => ({
+                        value: s._id,
+                        label: s.name,
+                    }))}
+                    defaultValue={filters.style}
+                />
+            </ProductCatalog>
         </>
     );
 }
