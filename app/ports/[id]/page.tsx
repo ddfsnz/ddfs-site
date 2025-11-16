57e0f76ab6378a9ad69a8acd87a778c41f2fe9e4
jcollicoat <jcollicoat@gmail.com>
2025-11-16 19:43:47 +0000

    product page top padding

--- app/ports/[id]/page.tsx
+++ app/ports/[id]/page.tsx
@@ -60,7 +60,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     );
 
     return (
-        <main className="mx-auto max-w-7xl px-4 py-32 pt-40">
+        <main className="mx-auto max-w-7xl px-4 py-32 pt-24">
             <div className="grid grid-cols-1 gap-6">
                 <Breadcrumb>
                     <BreadcrumbList>
