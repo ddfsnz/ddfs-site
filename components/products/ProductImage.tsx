ee60b8a969a1944e1b0b0fbe7b5a759ec4a149fd
Joseph Collicoat <jcollicoat@gmail.com>
2026-07-26 14:12:59 +0100

    add thumbnails to product images

--- components/products/ProductImage.tsx
+++ components/products/ProductImage.tsx
@@ -1,19 +1,27 @@
 import { ImageIcon } from "lucide-react";
 import Image from "next/image";
 import { getImageSrc } from "@/lib/sanity-image";
+import { cn } from "@/lib/utils";
 import { ProductImage as ProductImageType } from "@/types/product";
 
 export function ProductImage({
     image,
     name,
+    className,
 }: {
     image?: ProductImageType[][number];
     name: string;
+    className?: string;
 }) {
     const imageSrc = image ? getImageSrc(image) : undefined;
 
     return (
-        <div className="flex aspect-square items-center justify-center overflow-hidden rounded-sm bg-white">
+        <div
+            className={cn(
+                "flex aspect-square items-center justify-center overflow-hidden rounded-sm bg-white",
+                className,
+            )}
+        >
             {/* TODO: support base 64 */}
             {imageSrc && !imageSrc.startsWith("data") ? (
                 <div className="relative h-full max-h-10/12 w-full max-w-10/12">
--- components/products/ProductImages.tsx
+++ components/products/ProductImages.tsx
@@ -1,31 +1,43 @@
 "use client";
 
 import "swiper/css";
-import { Scrollbar } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import { Thumbs } from "swiper/modules";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
 import { ProductImage } from "@/components/products/ProductImage";
 import { Product } from "@/types/product";
 
 export function ProductImages({ product }: { product: Product }) {
+    const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
+
     return (
-        <Swiper modules={[Scrollbar]}>
-            {product.images ? (
-                product.images.map((i, index) => (
+        <div className="relative">
+            <Swiper thumbs={{ swiper: thumbsSwiper }} modules={[Thumbs]} loop>
+                {product.images?.map((i, index) => (
                     <SwiperSlide key={i.asset._id}>
                         <ProductImage
                             image={i}
                             name={`${product.name} image ${index}`}
                         />
                     </SwiperSlide>
-                ))
-            ) : (
-                <SwiperSlide>
-                    <ProductImage
-                        image={product.images?.[0]}
-                        name={product.name}
-                    />
-                </SwiperSlide>
-            )}
-        </Swiper>
+                ))}
+            </Swiper>
+            <div className="absolute bottom-2 left-1/2 z-10 -translate-x-1/2">
+                <Swiper onSwiper={setThumbsSwiper}>
+                    {product.images?.map((i, index) => (
+                        <SwiperSlide
+                            key={i.asset._id}
+                            className="mx-0.5 size-10! cursor-pointer transition-opacity [&:not(.swiper-slide-thumb-active)]:opacity-50"
+                        >
+                            <ProductImage
+                                image={i}
+                                name={`${product.name} image ${index}`}
+                                className="size-10 border"
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </div>
     );
 }
